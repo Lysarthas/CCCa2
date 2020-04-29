3d84acc9c2eaf8592b7672f5bb021c671a8e4cb8
Karun Mathew <karun.varghesemathew@student.unimelb.edu.au>
2020-04-29 12:22:02 +1000

    Regenerated access token and secret

--- TwitterCrawler.py
+++ TwitterCrawler.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = 'h1dqkm4wP9vui7U3snEMS0Z6A'
-consumer_secret = 'm20fMBT1Ijx7yNS9PXf8hSmYix8TgkkNtp3YHin0EO7UWiROFZ'
-access_token = '1170264592410267648-SZUOk09ew5EAURbUcPvEi4PoyCGMB0'
-access_secret = 'EsgTim4FkLeYxmjX1baOUKbeGgj2YDwf2BknHkxLr1Tfu'
+consumer_key = 'cGrAaG5mkE6t4wnBbcUJ3PlrY'
+consumer_secret = 'm95G2ZzDoW45aQaK1KajBjYZBETtcWeJBkWNcuCoUgPJMbrWe4'
+access_token = '1170264592410267648-eeYlYJuUBfcd1B0fsm0EibgrCYnSMM'
+access_secret = 'tPq5g4FWN4xNxTyPANWdprpU4jeKBrmist2kniM5jXdmn'
 
 tweetsPerQry = 100
 maxTweets = 1000000
