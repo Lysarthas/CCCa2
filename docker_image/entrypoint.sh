984e6aa62eb4113c73c2490a4a3c1731708c1be5
jallychun <jallychun@outlook.com>
2020-05-09 04:55:52 +1000

    add docker-images-related files to git

---
+++ docker_image/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ -z "${pyscript}" ]]; then
+    echo "pyscript required. existing"
+    exit 1
+fi
+
+cd CCCa2
+git checkout couchdb_store
+git pull https://jalllychun:compgliCit5y@github.com/Lysarthas/CCCa2.git couchdb_store
+
+pip install -r /CCCa2/requirements.txt
+
+python $pyscript
