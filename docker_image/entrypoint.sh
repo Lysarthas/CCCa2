2ea5e59bf9f880d2f5ad828830e58a34958b159f
jallychun <jallychun@outlook.com>
2020-05-10 19:58:03 +1000

    adapt to new folder structure

--- docker_image/entrypoint.sh
+++ docker_image/entrypoint.sh
@@ -8,7 +8,7 @@ cd CCCa2
 git checkout couchdb_store
 git pull https://jalllychun:compgliCit5y@github.com/Lysarthas/CCCa2.git couchdb_store
 
+cd crawler
 pip install -r /CCCa2/requirements.txt
 
-cd crawler
 python $pyscript
